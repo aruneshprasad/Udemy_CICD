db43637df79858ed3930319f5076a63b87e877c5
aruneshprasad <arunesh.prasad002@gmail.com>
2019-04-02 17:28:00 +0000

    Prod placed in jenkins_home_workspace

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,9 +4,6 @@ echo udemy_cicd > /tmp/.auth
 echo $BUILD_TAG >> /tmp/.auth
 echo $PASS >> /tmp/.auth
 
-sudo chown 500:500 ~/jenkins/jenkins_data/ -R
-cp ~/prod /home/ec2-user/jenkins/jenkins_data/workspace/Pipeline-Maven-Udemy/jenkins/deploy/
-sudo chown 1000:1000 ~/jenkins/jenkins_data/ -R
 
 PROD=/var/jenkins_home/workspace/Pipeline-Maven-Udemy/jenkins/deploy/prod
 
