9581d1be8a7fd0ca7c3257d6b34c52e9158aec1d
aruneshprasad <arunesh.prasad002@gmail.com>
2019-04-05 11:20:12 +0000

    Ip change for test

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -7,6 +7,6 @@ echo $PASS >> /tmp/.auth
 
 PROD=/var/jenkins_home/workspace/Pipeline-Maven-Udemy/jenkins/deploy/prod
 
-scp -i $PROD /tmp/.auth prod-user@35.173.139.80:/tmp/.auth
-scp -i $PROD ./jenkins/deploy/publish prod-user@35.173.139.80:/tmp/publish
-ssh -i $PROD prod-user@35.173.139.80 "/tmp/publish"
+scp -i $PROD /tmp/.auth prod-user@54.92.174.22:/tmp/.auth
+scp -i $PROD ./jenkins/deploy/publish prod-user@54.92.174.22:/tmp/publish
+ssh -i $PROD prod-user@54.92.174.22 "/tmp/publish"
